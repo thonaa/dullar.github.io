505906d36a0d3bb4b9c1d96bb7934299f7e16ee0
Just be free <justbefree@126.com>
2021-06-03 10:37:20 +0800

    Set default branch as master

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 # @Author: Just be free
 # @Date:   2021-06-02 15:20:12
 # @Last Modified by:   Just be free
-# @Last Modified time: 2021-06-02 15:22:34
+# @Last Modified time: 2021-06-03 10:36:47
 
 # 当发生错误时中止脚本
 set -e
@@ -21,7 +21,7 @@ git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-git push -f git@github.com:dullarjs/dullar.github.io.git main:gh-pages
+git push -f git@github.com:dullarjs/dullar.github.io.git master:gh-pages
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
