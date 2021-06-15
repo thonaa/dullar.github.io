dd966f0f80a93d7f7fb3a0cb7a976bf849e836e6
Just be free <justbefree@126.com>
2021-06-15 10:48:08 +0800

    修改打包路径

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 # @Author: Just be free
 # @Date:   2021-06-02 15:20:12
 # @Last Modified by:   Just be free
-# @Last Modified time: 2021-06-03 10:36:47
+# @Last Modified time: 2021-06-15 10:34:30
 
 # 当发生错误时中止脚本
 set -e
@@ -10,17 +10,27 @@ set -e
 # 构建
 npm run build-storybook
 
+npm run build
+
+mkdir dist/docs
+
+mv storybook-static/* dist/docs
+
 # cd 到构建输出的目录下 
-cd storybook-static
+cd dist
+
+
 
 # 部署到自定义域域名
 # echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
 git push -f git@github.com:dullarjs/dullar.github.io.git master:gh-pages
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+/*
+* @Author: Just be free
+* @Date:   2021-06-15 09:57:18
+* @Last Modified by:   Just be free
+* @Last Modified time: 2021-06-15 09:59:02
+* @E-mail: justbefree@126.com
+*/
+const publicPath = process.env.NODE_ENV === 'production' ? "/dullar.github.io/" : "/";
+module.exports = {
+  publicPath
+}
