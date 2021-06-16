31ffd3ce078cb44c824798df88cccfa1558704d0
Just be free <justbefree@126.com>
2021-06-16 09:41:12 +0800

    注释publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,10 @@
 * @Author: Just be free
 * @Date:   2021-06-15 09:57:18
 * @Last Modified by:   Just be free
-* @Last Modified time: 2021-06-15 09:59:02
+* @Last Modified time: 2021-06-16 09:39:10
 * @E-mail: justbefree@126.com
 */
-const publicPath = process.env.NODE_ENV === 'production' ? "/dullar.github.io/" : "/";
+// const publicPath = process.env.NODE_ENV === 'production' ? "/dullar.github.io/" : "/";
 module.exports = {
-  publicPath
+  // publicPath
 }
