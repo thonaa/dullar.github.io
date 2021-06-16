426f3657471bcf58e6e7a4437820c2a4eb23f61d
Just be free <justbefree@126.com>
2021-06-16 10:48:43 +0800

    修改文案

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img class="logo" alt="dullar logo" src="./assets/3@2x.png">
+  <Solgan/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Solgan from './components/Solgan.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Solgan
   }
 }
 </script>
@@ -21,6 +21,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+body {
+  padding: 0;
+  margin: 0;
+}
+.logo {
+  margin: 0;
+  padding:  0;
+
 }
 </style>
---
+++ src/components/Solgan.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="solgan">
+    <h1>Dollar moves the technology</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Solgan'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
