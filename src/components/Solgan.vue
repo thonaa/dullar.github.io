3c72a0d6f7980a0ec453041ed1c54c62312bbda9
Just be free <justbefree@126.com>
2021-07-26 14:46:43 +0800

    docs: add @dullar/mercury component lib

--- src/components/Solgan.vue
+++ src/components/Solgan.vue
@@ -31,6 +31,10 @@
           <span> Advanced and highly reusable Mobile UI Components built on <a href="https://v3.vuejs.org/" target="_blank">Vue3</a></span>
         </li>
         <li>
+          <a href="http://ui.dullar.xyz/mercury/#/" target="_blank">@dullar/mercury:</a>
+          <span> Advanced and highly reusable UI Components built on <a href="https://vuejs.org/" target="_blank">Vue2</a></span>
+        </li>
+        <li>
           <a href="https://www.npmjs.com/package/@dullar/cube" target="_blank">@dullar/cube:</a>
           <span> A modern JavaScript utility library</span>
         </li>
